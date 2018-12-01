541a98456259c8e41f35a2ec8652f74a79b3caeb
Angheng <bungae1112@gmail.com>
2018-12-01 20:10:53 +0900

    test

--- batting_servo.py
+++ batting_servo.py
@@ -7,9 +7,9 @@ class Batting_Servo(object):
     def __init__(self, bus_number=1):
         self.wheel = Servo.Servo(self.channel, bus_number=bus_number, offset=10)
         self.wheel.setup()
-        self.set_default()
+        self.set_position()
 
-    def set_default(self, angle=-150):
+    def set_position(self, angle=-150):
         self.wheel.write(angle)
 
     def batting(self, angle=270):
@@ -18,5 +18,20 @@ class Batting_Servo(object):
 
 if __name__ == "__main__":
     bat = Batting_Servo()
-    sleep(1)
-    bat.batting()
+
+    import RPi.GPIO as GPIO
+
+    btnPin = 7
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(btnPin, GPIO.In)
+
+    before_input = 0
+    while True:
+        button_input = GPIO.input(btnPin)
+
+        if button_input is 1:
+            bat.batting()
+            bat.set_position()
+        if button_input != before_input:
+            print(button_input)
+        before_input = button_input
