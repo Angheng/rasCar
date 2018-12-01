3f4d6926fe7da2145b8402ee683fd4e03edc3ac4
Angheng <bungae1112@gmail.com>
2018-12-01 19:50:43 +0900

    test

--- batting_servo.py
+++ batting_servo.py
@@ -5,7 +5,7 @@ class Batting_Servo(object):
     channel = 1
 
     def __init__(self, bus_number=1):
-        self.wheel = Servo.Servo(self._channel, bus_number=bus_number, offset=10)
+        self.wheel = Servo.Servo(self.channel, bus_number=bus_number, offset=10)
         self.wheel.setup()
 
     def test(self, angle=10):
