5531fb4383cd2e35f960a44ccc6016c88c040abd
Angheng <bungae1112@gmail.com>
2018-12-01 19:50:01 +0900

    test

--- batting_servo.py
+++ batting_servo.py
@@ -4,14 +4,14 @@ from PCA9685 import Servo
 class Batting_Servo(object):
     channel = 1
 
-    def __init__(self, bus_number = 1):
+    def __init__(self, bus_number=1):
         self.wheel = Servo.Servo(self._channel, bus_number=bus_number, offset=10)
         self.wheel.setup()
 
-    def test(self):
-        self.wheel.write(10)
+    def test(self, angle=10):
+        self.wheel.write(angle)
 
 
 if __name__ == "__main__":
-    bat = Batting_Servo
+    bat = Batting_Servo()
     bat.test()
