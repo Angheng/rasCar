58cd7208e4b599d0b558c7b23841a92a68b68c91
Angheng <bungae1112@gmail.com>
2018-12-01 19:47:50 +0900

    test

--- batting_servo.py
+++ batting_servo.py
@@ -8,7 +8,10 @@ class Batting_Servo(object):
         self.wheel = Servo.Servo(self._channel, bus_number=bus_number, offset=10)
         self.wheel.setup()
 
+    def test(self):
+        self.wheel.write(10)
+
 
 if __name__ == "__main__":
     bat = Batting_Servo
-    bat.wheel.write(10)
+    bat.test()
